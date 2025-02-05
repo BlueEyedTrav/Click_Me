dd4cbe18dce8afc7ac77fb5b5564d91ae5f81736
BlueEyedTrav <bubbagourley58@gmail.com>
2025-02-04 20:16:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // script.js
 
 document.getElementById('Button').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'John 3:16
-16 For God so loved the world, that he gave his only begotten Son that whosoever believeth In him should not perish, but have everlasting life.';
+    document.getElementById('message').textContent = ' John 3:16.';
 });

dc9bcd3f9afedb1d38b39e82e67b364babe05875
BlueEyedTrav <bubbagourley58@gmail.com>
2025-02-04 21:06:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // script.js
 
-document.getElementById('John 3:16
-King James Version
-16 For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.Button').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'John 3:16
-King James Version
-16 For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.';
+document.getElementById('goodBoyButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Good boy!';
 });
